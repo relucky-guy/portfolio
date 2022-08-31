0aa388f9c704c5c57cc3bf530f4f4c30eccffaac
Vladislav <vlad@bannikon.ru>
2022-08-31 22:00:08 +0300

    ready three screens

---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+// const gulp        = require('gulp');
+// const browserSync = require('browser-sync');
+// const sass = require('gulp-sass')(require('sass'));
+// const cleanCSS = require('gulp-clean-css');
+// const autoprefixer = require('gulp-autoprefixer');
+// const rename = require("gulp-rename");
+
+// gulp.task('server', function() {
+
+//     browserSync({
+//         server: {
+//             baseDir: "src"
+//         }
+//     });
+
+//     gulp.watch("src/*.html").on('change', browserSync.reload);
+// });
+
+// gulp.task('styles', function() {
+//     return gulp.src("src/sass/**/*.+(scss|sass)")
+//         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//         .pipe(rename({suffix: '.min', prefix: ''}))
+//         .pipe(autoprefixer())
+//         .pipe(cleanCSS({compatibility: 'ie8'}))
+//         .pipe(gulp.dest("src/css"))
+//         .pipe(browserSync.stream());
+// });
+
+// gulp.task('watch', function() {
+//     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+// })
+// 
+// gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+
+const { src, dest, parallel, series, watch, on } = require('gulp');
+const browserSync  = require('browser-sync');
+const sass         = require('gulp-sass')(require('sass'));
+const cleanCSS     = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename       = require("gulp-rename");
+const imagemin     = require('gulp-imagemin');
+const htmlmin      = require('gulp-htmlmin');
+
+function server() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+};
+
+function styles() {
+    return src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest("src/css"))
+        .pipe(browserSync.stream());
+};
+
+function html() {
+    return src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest("dist/"));
+};
+
+function scripts() {
+    return src("src/js/**/*.js")
+        .pipe(dest("dist/js"))
+        .pipe(browserSync.stream());
+};
+
+function fonts() {
+    return src("src/fonts/**/*")
+        .pipe(dest("dist/fonts"))
+        .pipe(browserSync.stream());
+};
+
+function icons() {
+    return src("src/icons/**/*")
+        .pipe(dest("dist/icons"))
+        .pipe(browserSync.stream());
+};
+
+function images() {
+    return src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(dest("dist/img"))
+        .pipe(browserSync.stream());
+};
+
+function startwatch() {
+    watch("src/sass/**/*.+(scss|sass)", styles);
+    watch("src/*.html").on('change', browserSync.reload);
+    watch("src/*.html").on('change', html);
+    watch("src/js/**/*.js").on('change', scripts);
+    watch("src/fonts/**/*").on('all', fonts);
+    watch("src/icons/**/*").on('all', icons);
+    watch("src/img/**/*").on('all', images);
+}
+
+exports.server  = server;
+exports.styles  = styles;
+exports.html    = html;
+exports.scripts = scripts;
+exports.fonts   = fonts;
+exports.icons   = icons;
+exports.images  = images;
+
+exports.default = parallel(styles, html, scripts, server, fonts, icons, images, startwatch);     
