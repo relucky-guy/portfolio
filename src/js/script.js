5c2d4276dd7d51d9751cb184a96af246eed96771
Vladislav <vlad@bannikon.ru>
2022-09-01 21:42:20 +0300

    added resume, skills, portfolio and price for desctop

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__percent'),
+      lines   = document.querySelectorAll('.skills__scale span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
